9c5a494fcba6e70fbc76e6f65be2816485d8ef5d
shreyapatill <70827966+shreyapatill@users.noreply.github.com>
2020-09-28 14:06:42 -0400

    Add files via upload

---
+++ EF1 Algorithm/src/EF1Algo.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EF1Algo {
+
+	public List<List<Integer>> allocate(final List<List<Integer>> agentValues) {
+		int agents = agentValues.size();
+		int items = agentValues.get(0).size();
+
+		List<List<Integer>> allocations = new ArrayList<>();
+
+		for (int i = 0; i < agents; i++) { // creates list objects to hold each agent's allocations
+			allocations.add(new ArrayList<>());
+		}
+		int diff = items-agents;
+		while (items > diff ) { //check that # of items doesn't reach 0
+			for (int i = 0; i < agents; i++) {
+				int bestIndex = findBestItem(agentValues.get(i));
+				int bestVal = agentValues.get(i).get(bestIndex);
+				allocations.get(i).add(bestVal); // adds best item to allocation list
+
+				for (int x = 0; x < agents; x++) {
+					agentValues.get(x).remove(bestIndex); // removes item from other agent's lists
+				}
+				--items;
+			}
+			
+		}
+
+		printAllocations(allocations);
+
+		return allocations;
+	}
+
+	public Integer findBestItem(List<Integer> agentI) { // iterates through agent's list of values to find the best item
+		int maxIndex = 0;
+
+		for (int i = 1; i < agentI.size(); i++) {
+			if (agentI.get(maxIndex) < agentI.get(i)) {
+				maxIndex = i;
+			}
+		}
+		return maxIndex;
+	}
+
+	public void printAllocations(List<List<Integer>> allocations) {
+		for (List<Integer> agentI : allocations) {
+			for (int val : agentI) {
+				System.out.print(val + ", ");
+			}
+			System.out.println();
+		}
+	}
+
+}

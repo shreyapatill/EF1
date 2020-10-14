9ecebd32da0dbc6987ab5cf1f7e3385957f80a26
shreyapatill <70827966+shreyapatill@users.noreply.github.com>
2020-10-14 14:18:44 -0400

    Update MMSAlgo.java

--- MMS Algo/src/MMSAlgo.java
+++ MMS Algo/src/MMSAlgo.java
@@ -8,76 +8,60 @@ public class MMSAlgo {
 
 		List<Float> aveVals = new ArrayList<>();
 		aveVals = getAverages(agentValues, agents);
-		
+
 		List<List<Integer>> allocations = new ArrayList<>();
-		
-		for (int i = 0; i < agents; i++) { // creates list objects to hold each agent's allocations
+
+		for (int agentI = 0; agentI < agents; agentI++) { // creates list objects to hold each agent's allocations
 			allocations.add(new ArrayList<>());
 		}
 
 		while (items > 0) { // check that # of items doesn't reach 0
 			int agentNum = 0;
-			for (int i = 0; i < agents; i++) {
+			for (int agentIndex = 0; agentIndex < agents; agentIndex++) {
 				if (items == 0) {
 					break;
 				}
-				
-				for (int j = 0; j < items; j++) {
-					int tempVal = agentValues.get(i).get(j);
-					
-					if( tempVal >= aveVals.get(i)/2 ) { //checks if agent valuation is greater than average/2
+
+				for (int itemIndex = 0; itemIndex < items; itemIndex++) {
+					int tempVal = agentValues.get(agentIndex).get(itemIndex);
+
+					if (tempVal >= aveVals.get(agentIndex) / 2) { // checks if agent valuation is greater than average/2
 						allocations.get(agentNum).add(tempVal);
-						
+
 						for (int x = 0; x < agents; x++) {
-							agentValues.get(x).remove(j); // removes item from other agent's lists
+							agentValues.get(x).remove(itemIndex); // removes item from other agent's lists
 						}
-						
-						agentValues.remove(i); //removes agent from list
+
+						agentValues.remove(agentIndex); // removes agent from list
+						aveVals.remove(agentIndex);
 						agents--;
-						i--;
+
+						agentIndex--;
 						aveVals = getAverages(agentValues, agents);
 						break;
 					}
 				}
-
 				agentNum++;
 				--items;
 			}
-
+			break;
 		}
-
-		//step 3
-		while (items > 0) { // check that # of items doesn't reach 0
-			for (int i = 0; i < allocations.size() ; i++) {
-				if (items == 0) {
-					break;
-				}
-				
-				if( allocations.get(i).size()==0 ) {
-					int bestIndex = findBestItem(agentValues.get(i));
-					int bestVal = agentValues.get(i).get(bestIndex);
-					allocations.get(i).add(bestVal); // adds best item to allocation list
-	
-					for (int x = 0; x < agents; x++) {
-						agentValues.get(x).remove(bestIndex); // removes item from other agent's lists
-					}
-					--items;
-				}
-			}
-		}
-		
-		printAllocations( allocations );
+		printAllocations( allocations );		
 	}
-	
-	public Integer findBestItem(List<Integer> agentI) { // iterates through agent's list of values to find the best item
-		int maxIndex = 0;
 
-		for (int i = 1; i < agentI.size(); i++) {
-			if (agentI.get(maxIndex) < agentI.get(i)) {
-				maxIndex = i;
+	public List<Float> getAverages(List<List<Integer>> agentValues, int agents) {
+		// finds sum of all values
+		List<Float> aveVal = new ArrayList<>();
+
+		for (List<Integer> agentI : agentValues) {
+			float sum = 0;
+			for (int v : agentI) {
+				sum += v;
 			}
+			aveVal.add(sum / agents);
 		}
-		return maxIndex;
+
+		return aveVal;
 	}
 
 	public void printAllocations(List<List<Integer>> allocations) {
@@ -94,18 +78,4 @@ public class MMSAlgo {
 		}
 	}
 
-	public List<Float> getAverages(List<List<Integer>> agentValues, int agents) {
-		//finds sum of all values
-		List<Float> aveVal = new ArrayList<>();
-		
-		for (List<Integer> agentI : agentValues) {
-			float sum = 0;
-			for (int v : agentI) {
-				sum += v;
-			}
-			aveVal.add(sum/agents);
-		}
-		
-		return aveVal;
-	}
 }
